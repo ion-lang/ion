b8fd65fede4f062f428aa448a4faa936f6c9e79c
Juan Lulkin <maiz@lulk.in>
2016-09-11 14:42:11 +0300

    Adds some colors to errors

--- helpers/prettify.js
+++ helpers/prettify.js
@@ -1,15 +1,18 @@
 const R = require('ramda')
+const colors = require('colors/safe')
 
 const mapWithIndex = R.addIndex(R.map)
 const compact = R.reject(R.isNil)
-const dashes = R.times(() => '-')
+const dots = R.times(R.always('.'))
+const spaces = R.times(R.always(' '))
 
 module.exports = function prettify (code, location) {
   return '...\n' + compact(mapWithIndex((line, idx) => {
     if (location.start.line - 6 > idx ||
       location.start.line + 6 < idx) return
     return idx + 1 === location.start.line
-      ? `${idx} ${line}\n${dashes(location.start.column + 2).join('')}^`
-      : `${idx} ${line}`
+      ? colors.red.inverse(` ${idx + 1} | ${line}`)
+        + "\n" + spaces(5).join('') + colors.dim(`${dots(location.start.column - 1).join('')}^`)
+      : colors.gray(` ${idx + 1} | `) +  line
   }, code.split('\n'))).join('\n') + '\n...'
 }
--- parser.js
+++ parser.js
@@ -1,16 +1,14 @@
 const prettify = require('./helpers/prettify')
 const ion = require('./ion')
+const colors = require('colors/safe')
 
 module.exports = (program) => {
   try {
     return ion.parse(program)
   } catch (e) {
     if (e.location) {
+      console.log("\n", colors.bold.red(e.name), colors.dim(e.message))
       console.log(prettify(program, e.location))
-      console.log('---')
-      console.log(e.message)
-      console.log('---')
-      console.log(e.name)
     } else {
       console.log(e)
     }
